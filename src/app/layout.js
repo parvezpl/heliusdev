fc5733a9a8ea703a5414b8976e1096c11c5142d5
helius-dev <heliustime@gmail.com>
2025-04-07 08:02:16 +0530

    chrome verifiy1

--- src/app/layout.js
+++ src/app/layout.js
@@ -22,11 +22,14 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <meta name="google-site-verification" content="ABi5tu30kKpHJMlDdhW71AwFIggm8tsK2VFLhqw9F-8" />
+      </head>
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
-          <Header/>
+        <Header />
         <div className="main_page">
-            <MainPageNav />
-        {children}
+          <MainPageNav />
+          {children}
         </div>
         <Footer />
       </body>
