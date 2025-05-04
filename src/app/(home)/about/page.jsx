62ce610d2d30ca66abd10f5eaaf8aedbefb3a7b2
helius-dev <heliustime@gmail.com>
2025-05-04 18:40:14 +0530

    add log portfolio

--- src/app/(home)/about/page.jsx
+++ src/app/(home)/about/page.jsx
@@ -1,14 +1,17 @@
 import Link from "next/link";
-import "./about.css";
+import Portfolio from "./portfolio";
 export default function about(params) {
     return (
-        <div className=" flex h-screen box-border text-black bg-gray-300 pt-10 sm:pt-0 justify-center items-center text-[2vw] ">
+        <div className="flex flex-col h-full box-border text-black bg-gray-300  justify-center items-center">
             <div className=" justify-center items-center ">
-                <p>
-                    <span>HeliusDEV</span> is a web development platform specializing in modern and efficient solutions.
+                <p className="mx-4">
+                    <span className="text-red-600 font-bold">HeliusDEV</span> is a web development platform specializing in modern and efficient solutions.
                     It focuses on building dynamic and scalable web applications using React.js and Next.js for the frontend, while leveraging Node.js with Express for the backend.
                     In addition to web development, HeliusDEV also creates custom Chrome extensions to enhance browser functionality and user experience.
                 </p>
+            </div>
+            <div className="flex w-full">
+                <Portfolio/>
             </div>
         </div>
     )
