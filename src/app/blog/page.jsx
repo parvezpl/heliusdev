1a5d24374d6d6ab375ccfcce978095776e127666
helius-dev <heliustime@gmail.com>
2025-05-04 16:10:37 +0530

    blog area

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -7,7 +7,7 @@ function page() {
     const [getDatas, setDatas] = useState([null])
     const [formData, setFormData] = useState({ title: '', content: '' });
     const [getLocalData, setLocalData] = useState(
-        {user:null}
+        { user: null }
     )
 
     const handleChange = (e) => {
@@ -15,19 +15,19 @@ function page() {
     };
     useEffect(() => {
         const data = localStorage.getItem('user')
-        if(data){
+        if (data) {
             setLocalData(JSON.parse(data))
-        }else{
-          null  
+        } else {
+            null
         }
-    },[])
+    }, [])
     const submithandler = async (e) => {
         e.preventDefault()
-        console.log(getLocalData.user)
+        // console.log(getLocalData.user)
         const res = await fetch('/api/blog', {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify({ ...formData, user:getLocalData.user }),
+            body: JSON.stringify({ ...formData, user: getLocalData.user }),
         });
         const result = await res.json();
         if (result) {
@@ -35,44 +35,43 @@ function page() {
         }
     }
     const getData = async () => {
-        const res = await fetch('/api/blog', {
-            method: 'GET',
-            // headers: { 'Content-Type': 'application/json' },
-        })
+        const res = await fetch(`/api/blog?id=${getLocalData.user._id}`)
         const result = await res.json();
         if (result) {
-            console.log("get", result.data)
-            setDatas(result.data)
+            // console.log("get", result.data.blogs)
+            setDatas(result.data.blogs)
         }
     }
     return (
         <>
             <ManiNav />
-            <div className='flex flex-col m-8 items-center w-screen h-screen'>
-                <h1>blog page</h1>
-                <form className='flex flex-col gap-4 m-4' onSubmit={submithandler}>
+            <div className='flex flex-col m-8 items-center  min-h-screen '>
+                <h1 className=' capitalize text-2xl'>blog page</h1>
+                <form className='flex flex-col gap-4 m-4 border w-[70%] p-4 h-fit justify-center items-center shadow-black shadow-md ' onSubmit={submithandler}>
                     <input type="text" placeholder="title"
                         name="title"
                         onChange={handleChange}
                         value={formData.name}
-                        className=' outline-1' />
-                    <input type="text" placeholder="content"
+                        className=' outline-1 w-[40vw]' />
+                    <textarea type="text" placeholder="content"
                         name="content"
                         onChange={handleChange}
                         value={formData.name}
-                        className=' outline-1' />
+                        className=' outline-1 w-[40vw] min-h-[200px]' />
                     <button type="submit"
-                        className=" cursor-pointer border w-fit hover:bg-blue-200 px-2">save</button>/
+                        className="  cursor-pointer border w-fit hover:bg-blue-200 px-2">save</button>
                 </form>
-                <div>
-                    <h1>get blog data</h1>
-                    <button onClick={() => getData()} className='cursor-pointer border w-fit hover:bg-blue-200 px-2'>get</button>
+                <div className='bg-gray-400 flex flex-col min-h-screen w-full p-2'>
+                    <div className='flex flex-row gap-4'>
+                        <h1>get blog data</h1>
+                        <button onClick={() => getData()} className='cursor-pointer border w-fit hover:bg-blue-200 px-2'>get</button>
+                    </div>
                     <div className='flex flex-col'>
                         {
                             getDatas?.map(res => {
                                 if (res) {
                                     return (
-                                        <div key={res?._id} className='flex p-4 border'>{
+                                        <div key={res?._id} className='flex flex-col p-4 border w-full'>{
                                             Object.keys(res).map((data) => {
                                                 return (
                                                     <div key={data} className='p-4'>{res[data]}</div>
